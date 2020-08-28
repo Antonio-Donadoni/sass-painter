09660cce7e998cb3276dcc3600eb4ff752a8de67
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-08-28 15:56:38 +0200

    fix gitignore

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,6 @@ let mix = require('laravel-mix');
 mix
 .js('src/app.js', 'dist/')
 .sass('src/app.scss', 'dist/')
-.copy('src/*.html', 'dist/');
+.copy('src/*.html', 'dist/')
+.copyDirectory('src/imgs', 'dist/imgs')
+;
